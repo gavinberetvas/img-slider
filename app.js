3fc09501a4263039ff5882c97f95e8e3afbb3147
Gavin <gavinberetvas@ucsb.edu>
2023-06-01 23:34:47 -0700

    complete

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 /* eslint-disable quotes */
 /* eslint-disable require-jsdoc */
+let counter = 0;
 const leftArrow = document.getElementById("arrow-left");
 const rightArrow = document.getElementById("arrow-right");
 const imgWrapper = document.querySelector(".img-wrapper");
-let counter = 0;
+const radioButtons = document.querySelectorAll(".radio");
+let intervalFunction = setInterval(advance, 2000);
 
-leftArrow.addEventListener("click", function () {
-  counterDown();
-  moveElement();
-});
+initializeArrows();
+initializePlayPause();
+initializeRadioButtons();
 
-rightArrow.addEventListener("click", function () {
-  counterUp();
-  moveElement();
-});
+function initializeRadioButtons() {
+  radioButtons.forEach(function (radioButton) {
+    radioButton.addEventListener("click", function () {
+      counter = parseInt(radioButton.dataset.id, 10);
+      moveElement();
+    });
+  });
+}
 
-let radioButtons = document.querySelectorAll(".radio");
-
-radioButtons.forEach(function (radioButton) {
-  radioButton.addEventListener("click", function () {
-    counter = parseInt(radioButton.dataset.id, 10);
+function initializeArrows() {
+  leftArrow.addEventListener("click", function () {
+    counterDown();
     moveElement();
   });
-});
+  rightArrow.addEventListener("click", function () {
+    counterUp();
+    moveElement();
+  });
+}
+
+function initializePlayPause() {
+  document.getElementById('play').addEventListener('click', () => {
+    intervalFunction = setInterval(advance, 2000);
+    console.log("play");
+  });
+  document.getElementById('pause').addEventListener('click', () => {
+    clearInterval(intervalFunction);
+    console.log("pause");
+  });
+}
 
 function counterUp() {
   counter++;
@@ -35,17 +53,17 @@ function counterDown() {
 function moveElement() {
   if (counter < 0) {
     counter = 4;
-    imgWrapper.style.marginLeft = "-100rem";
+    imgWrapper.style.marginLeft = "-280rem";
   } else if (counter == 0) {
     imgWrapper.style.marginLeft = "0rem";
   } else if (counter == 1) {
-    imgWrapper.style.marginLeft = "-25rem";
+    imgWrapper.style.marginLeft = "-70rem";
   } else if (counter == 2) {
-    imgWrapper.style.marginLeft = "-50rem";
+    imgWrapper.style.marginLeft = "-140rem";
   } else if (counter == 3) {
-    imgWrapper.style.marginLeft = "-75rem";
+    imgWrapper.style.marginLeft = "-210rem";
   } else if (counter == 4) {
-    imgWrapper.style.marginLeft = "-100rem";
+    imgWrapper.style.marginLeft = "-280rem";
   } else if (counter > 4) {
     counter = 0;
     imgWrapper.style.marginLeft = "0rem";
@@ -54,8 +72,15 @@ function moveElement() {
   moveRadioButton();
 }
 
+function advance() {
+  counterUp();
+  moveElement();
+}
+
 function moveRadioButton() {
-  var radioBtn = document.querySelector('input[type="radio"][data-id="' + counter + '"]');
+  var radioBtn = document.querySelector(
+    'input[type="radio"][data-id="' + counter + '"]'
+  );
   if (radioBtn) {
     radioBtn.checked = true;
   }
