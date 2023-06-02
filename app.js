58a18d46622d0b4c76ae4bbde63b56426ef260b6
Gavin <gavinberetvas@ucsb.edu>
2023-06-01 22:51:29 -0700

    slider refactor and radio buttons work and track elements

--- app.js
+++ app.js
@@ -1,77 +1,63 @@
-const leftArrow = document.getElementById('arrow-left');
-const RightArrow = document.getElementById('arrow-right');
-const imgWrapper = document.querySelector('.img-wrapper');
-
-leftArrow.addEventListener('click', moveElementsLeft);
-RightArrow.addEventListener('click', moveElementsRight);
+/* eslint-disable quotes */
+/* eslint-disable require-jsdoc */
+const leftArrow = document.getElementById("arrow-left");
+const rightArrow = document.getElementById("arrow-right");
+const imgWrapper = document.querySelector(".img-wrapper");
 let counter = 0;
 
+leftArrow.addEventListener("click", function () {
+  counterDown();
+  moveElement();
+});
 
-// -4 -3 -2 -1 0 1 2 3 4
-// -25 -50 -75 -100 0 -25 -50 -75 -100
+rightArrow.addEventListener("click", function () {
+  counterUp();
+  moveElement();
+});
 
+let radioButtons = document.querySelectorAll(".radio");
 
-function moveElementsLeft() {
+radioButtons.forEach(function (radioButton) {
+  radioButton.addEventListener("click", function () {
+    counter = parseInt(radioButton.dataset.id, 10);
+    moveElement();
+  });
+});
+
+function counterUp() {
   counter++;
-  if (counter == -1) {
-    imgWrapper.style.marginLeft = '-100rem';
-} else if (counter == -2) {
-    imgWrapper.style.marginLeft = '-75rem';
-} else if (counter == -3) {
-    imgWrapper.style.marginLeft = '-50rem';
-} else if (counter == -4) {
-    imgWrapper.style.marginLeft = '-25rem';
-} else if (counter < -4) {
-    imgWrapper.style.marginLeft = '0rem';
-    counter = 0;
-}   else if (counter == 0) {
-    imgWrapper.style.marginLeft = '0rem';
 }
-  else if (counter == 1) {
-    imgWrapper.style.marginLeft = '-25rem';
+
+function counterDown() {
+  counter--;
+}
+
+function moveElement() {
+  if (counter < 0) {
+    counter = 4;
+    imgWrapper.style.marginLeft = "-100rem";
+  } else if (counter == 0) {
+    imgWrapper.style.marginLeft = "0rem";
+  } else if (counter == 1) {
+    imgWrapper.style.marginLeft = "-25rem";
   } else if (counter == 2) {
-    imgWrapper.style.marginLeft = '-50rem';
+    imgWrapper.style.marginLeft = "-50rem";
   } else if (counter == 3) {
-    imgWrapper.style.marginLeft = '-75rem';
-
+    imgWrapper.style.marginLeft = "-75rem";
   } else if (counter == 4) {
-    imgWrapper.style.marginLeft = '-100rem';
-
+    imgWrapper.style.marginLeft = "-100rem";
   } else if (counter > 4) {
     counter = 0;
-    imgWrapper.style.marginLeft = '0rem';
-}
-}
-
-function moveElementsRight() {
-    counter--;
-
-    if (counter == -1) {
-        imgWrapper.style.marginLeft = '-100rem';
-    } else if (counter == -2) {
-        imgWrapper.style.marginLeft = '-75rem';
-    } else if (counter == -3) {
-        imgWrapper.style.marginLeft = '-50rem';
-    } else if (counter == -4) {
-        imgWrapper.style.marginLeft = '-25rem';
-    } else if (counter < -4) {
-        imgWrapper.style.marginLeft = '0rem';
-        counter = 0;
-    }   else if (counter == 0) {
-        imgWrapper.style.marginLeft = '0rem';
-    }
-      else if (counter == 1) {
-        imgWrapper.style.marginLeft = '-25rem';
-      } else if (counter == 2) {
-        imgWrapper.style.marginLeft = '-50rem';
-      } else if (counter == 3) {
-        imgWrapper.style.marginLeft = '-75rem';
-    
-      } else if (counter == 4) {
-        imgWrapper.style.marginLeft = '-100rem';
-    
-      } else if (counter > 4) {
-        counter = 0;
-        imgWrapper.style.marginLeft = '0rem';
-    }
+    imgWrapper.style.marginLeft = "0rem";
   }
+  console.log(counter);
+  moveRadioButton();
+}
+
+function moveRadioButton() {
+  var radioBtn = document.querySelector('input[type="radio"][data-id="' + counter + '"]');
+  if (radioBtn) {
+    radioBtn.checked = true;
+  }
+}
+
